990b3512c0fc2893ca9873d4935e0091b24950ee
r-kolla <145572936+r-kolla@users.noreply.github.com>
2025-07-06 12:23:39 +0530

    fixed urls.py

--- project_backend/urls.py
+++ project_backend/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def root_redirect(request):
+    return redirect('/api/agent1/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('agent1.urls')), 
     path("api/agent1/", include("agent1.urls")),
+    path('', root_redirect),
 ]
