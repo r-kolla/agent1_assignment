f79558f1fce6efdc9c7de30dacf2a829e952ea2f
r-kolla <revanthkolla2@gmail.com>
2025-07-15 19:16:33 +0530

    fixed urls

--- agent1/urls.py
+++ agent1/urls.py
@@ -16,12 +16,12 @@ router.register(r'enquiries', EnquiryViewSet)
 
 urlpatterns = [
     path('classes/upcoming/', UpcomingClassList.as_view(), name='upcoming-classes'),
-    path('agent1/clients/search/', agent1_api_views.Agent1ClientSearchView.as_view(), name='agent1-client-search'),
-    path('agent1/orders/<str:id>/', agent1_api_views.Agent1OrderDetailView.as_view(), name='agent1-order-detail'),
-    path('agent1/courses/schedule/', agent1_api_views.Agent1CourseScheduleView.as_view(), name='agent1-course-schedule'),
-    path('agent1/services/', agent1_api_views.Agent1ServiceInfoView.as_view(), name='agent1-service-info'),
-    path('agent1/enquiries/', agent1_api_views.agent1_create_enquiry, name='agent1-create-enquiry'),
-    path("agent1/chat/", agent1_chat_api, name="agent1-chat"),
+    path('clients/search/', agent1_api_views.Agent1ClientSearchView.as_view(), name='agent1-client-search'),
+    path('orders/<str:id>/', agent1_api_views.Agent1OrderDetailView.as_view(), name='agent1-order-detail'),
+    path('courses/schedule/', agent1_api_views.Agent1CourseScheduleView.as_view(), name='agent1-course-schedule'),
+    path('services/', agent1_api_views.Agent1ServiceInfoView.as_view(), name='agent1-service-info'),
+    path('enquiries/', agent1_api_views.agent1_create_enquiry, name='agent1-create-enquiry'),
+    path('chat/', agent1_chat_api, name="agent1-chat"),
 
 ]
 
